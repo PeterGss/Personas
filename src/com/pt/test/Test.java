fb60d8a1e783e78ac8c00adee96e15e49025ab4a
Shaon <762290035@qq.com>
2018-08-27 09:30:46 +0800

    [chn] Test init

--- src/com/pt/test/Test.java
+++ src/com/pt/test/Test.java
@@ -2,6 +2,13 @@ package com.pt.test;
 
 import com.google.gson.Gson;
 import com.pt.personas.Bean;
+import eu.bitwalker.useragentutils.*;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 /**
  * Created by Shaon on 2018/8/24.
@@ -13,5 +20,95 @@ public class Test {
         String value ="{\"RecTime\":1526092689437,\"LogID\":36062315,\"LogType\":\"file_restore\",\"SrcIP\":\"172.16.33.64\",\"SrcPort\":41241,\"DstIP\":\"114.255.163.175\",\"DstPort\":80,\"sessionID\":\"19_1289418\",\"TransProto\":\"TCP\",\"AppProto\":\"HTTP\",\"TypeStr\":\"text/html\",\"Length\":389,\"Status\":\"REPEAT\",\"LocalUri\":\"/data/csmdp/data/var/http/19/235340\"}\n";
         bean = gson.fromJson(value,Bean.class);
         System.out.println(bean.toString());
+    }
+    public static void test1() {
+        List<String> userAgentList = null;
+        PrintWriter pw = null;
+        try {
+//			 userAgentList = FileUtils.readLines(new File("conf/useragent.txt"));
+//			 pw = new PrintWriter("output/out.txt");
+
+//			userAgentList = FileUtils.readLines(new File("conf/USERAGENT_ALL_001.txt"));
+//			pw = new PrintWriter("output/outAll-2.txt");
+
+            userAgentList = FileUtils.readLines(new File("conf/test.txt"));
+            pw = new PrintWriter("output/outtest.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (String string : userAgentList) {
+
+            string = getURLDecoderString(string);
+
+            UserAgent userAgent = UserAgent.parseUserAgentString(string);
+
+            OperatingSystem os = userAgent.getOperatingSystem();
+            String osName = os.getName();
+            String deType = os.getDeviceType().getName();
+            String osManufacturer = os.getManufacturer().getName();
+
+            Browser bwr = userAgent.getBrowser();
+            String bwrType = bwr.getBrowserType().getName();
+            String bwrName = bwr.getName();
+            String bwrEngine = bwr.getRenderingEngine().name();
+            String bwrManufacturer = bwr.getManufacturer().getName();
+
+            Version version = userAgent.getBrowserVersion();
+            String bwrVersion = null;
+            @SuppressWarnings("unused")
+            String bwrmajorVersion = null;
+            @SuppressWarnings("unused")
+            String bwrminorVersion = null;
+            if (version != null) {
+                bwrVersion = version.getVersion();
+                bwrmajorVersion = version.getMajorVersion();
+                bwrminorVersion = version.getMinorVersion();
+            }
+
+            Application app = userAgent.getApplication();
+            String appName = app.getName();
+            String appType = app.getApplicationType().getName();
+            String appManufacturer = app.getManufacturer().getName();
+
+            pw.println(string + " : ");
+            pw.print("    osName : " + osName);
+            pw.print(", osManufacturer : " + osManufacturer);
+            pw.print(", deType : " + deType);
+            pw.print(", bwrName : " + bwrName);
+            pw.print(", bwrEngine : " + bwrEngine);
+            pw.print(", bwrManufacturer : " + bwrManufacturer);
+            pw.print(", bwrType : " + bwrType);
+            pw.print(", bwrVersion : " + bwrVersion);
+            pw.print(", appName : " + appName);
+            pw.print(", appType : " + appType);
+            pw.print(", appManufacturer : " + appManufacturer);
+            pw.println();
+
+//			pw.println("    userAgent: " + userAgent);
+
+        }
+        pw.flush();
+        pw.close();
+    }
+
+    private static String getURLDecoderString(String str) {
+        String result = "";
+        if (null == str) {
+            return "";
+        }
+            try {
+                result = java.net.URLDecoder.decode(str, "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void test2() {
+        UserAgent userAgent1 = UserAgent.parseUserAgentString("1");
+        UserAgent userAgent2 = UserAgent.parseUserAgentString("mail.live.com");
+        System.out.println(userAgent1 == userAgent2);
+        System.out.println(userAgent1.equals(userAgent2));
     }
 }
